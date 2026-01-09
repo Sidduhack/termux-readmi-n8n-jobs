fdfb65a16de2db03c6b073847bcc63e5d60faff9
Sidduhack <157084733+Sidduhack@users.noreply.github.com>
2026-01-09 21:56:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
+import fs from "fs";
+
 import config from "./config.js";
 import fetchJobs from "./fetch/freejobalert.js";
 import qualFilter from "./filters/qualification.js";
-import dedupe from "./filters/dedup.js";
+import dedupe from "./filters/dedupe.js";
 import recentFilter from "./filters/daterecent.js";
 import todayFilter from "./filters/datetoday.js";
-import sendLog from "./actions/logger.js";
 
 import htmlAll from "./html/allnotification.js";
 import htmlLatest from "./html/latestfilter.js";
 import htmlToday from "./html/todayonly.js";
 
-import fs from "fs";
+import sendLog from "./actions/logger.js";
+import pushToGit from "./actions/github.js";
 
 export default async function run() {
-    sendLog("Automation started");
+  sendLog("Automation started");
 
-    let jobs = await fetchJobs(config.SOURCE_URL);
-    jobs = qualFilter(jobs, config.QUALIFICATIONS);
-    jobs = dedupe(jobs);
+  let jobs = [];
+  try {
+    jobs = await fetchJobs(config.SOURCE_URL);
+  } catch {
+    sendLog("Fetch failed completely");
+    return;
+  }
 
-    const recent = recentFilter(jobs, config.LOOKBACK_DAYS);
-    const today = todayFilter(jobs);
+  jobs = qualFilter(jobs, config.QUALIFICATIONS);
+  jobs = dedupe(jobs);
 
-    fs.writeFileSync("html/all.html", htmlAll(jobs));
-    fs.writeFileSync("html/latest.html", htmlLatest(recent));
-    fs.writeFileSync("html/today.html", htmlToday(today));
+  const recent = recentFilter(jobs, config.LOOKBACK_DAYS);
+  const today = todayFilter(jobs);
 
-    sendLog(`All:${jobs.length} Recent:${recent.length} Today:${today.length}`);
+  fs.writeFileSync("html/all.html", htmlAll(jobs));
+  fs.writeFileSync("html/latest.html", htmlLatest(recent));
+  fs.writeFileSync("html/today.html", htmlToday(today));
+
+  sendLog(`All:${jobs.length} Recent:${recent.length} Today:${today.length}`);
+
+  pushToGit("update job html");
 }
 
 run();
