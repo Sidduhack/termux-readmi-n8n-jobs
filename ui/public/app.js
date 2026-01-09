07cabb652c8845f1eb3b05e69bd7834d263251cd
Sidduhack <157084733+Sidduhack@users.noreply.github.com>
2026-01-09 21:57:44 +0530

    Update app.js

--- ui/public/app.js
+++ ui/public/app.js
@@ -1,6 +1,6 @@
 async function run() {
-    await fetch("/run");
-    const r = await fetch("/logs");
-    document.getElementById("logs").textContent = await r.text();
+  await fetch("/run");
+  const r = await fetch("/logs");
+  document.getElementById("logs").textContent = await r.text();
 }
 run();
