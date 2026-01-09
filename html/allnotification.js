c75930809686cb7d750152608e38a2ba53256ec4
Sidduhack <157084733+Sidduhack@users.noreply.github.com>
2026-01-09 21:55:10 +0530

    Update allnotification.js

--- html/allnotification.js
+++ html/allnotification.js
@@ -1,14 +1,12 @@
-export function buildAllJobsHTML(jobs) {
-    return `
-  <html><body>
-  <h1>All Jobs</h1>
-  <ul>
-    ${jobs.map(j => `
-      <li>
-        <b>${j.job_title}</b> |
-        ${j.qualification} |
-        <a href="${j.link}" target="_blank">Apply</a>
-      </li>`).join("")}
-  </ul>
-  </body></html>`;
+export default function htmlAll(jobs) {
+  return `
+<h1>All Jobs</h1>
+<ul>
+${jobs.map(j => `
+<li>
+<b>${j.job_title}</b> |
+${j.qualification} |
+<a href="${j.link}" target="_self">Apply</a>
+</li>`).join("")}
+</ul>`;
 }
