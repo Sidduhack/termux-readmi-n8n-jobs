fba1820fc00a43d72d69445225a17caaa1151a5e
Sidduhack <157084733+Sidduhack@users.noreply.github.com>
2026-01-09 21:52:52 +0530

    Refactor pushToGit function logging
    Updated logging message for git push attempts.

--- actions/github.js
+++ actions/github.js
@@ -2,11 +2,8 @@ import { exec } from "child_process";
 import sendLog from "./logger.js";
 
 export default function pushToGit(message = "auto update") {
-    exec(
-        `git add . && git commit -m "${message}" && git push`,
-        (err, stdout, stderr) => {
-            if (err) sendLog("Git push failed");
-            else sendLog("Git pushed successfully");
-        }
-    );
+  exec(
+    `git add . && git commit -m "${message}" && git push`,
+    () => sendLog("Git push attempted")
+  );
 }
